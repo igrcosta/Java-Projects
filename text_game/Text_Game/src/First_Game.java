9dc27d00cef649d2d0c39d0f4d54d2820c2e24f5
Igor Costa <igorcosta250705@gmail.com>
2025-03-18 20:54:03 -0300

    Adição secret name
    Adicionei a Feature do Zoio, escreva e descubra

--- text_game/Text_Game/src/First_Game.java
+++ text_game/Text_Game/src/First_Game.java
@@ -11,15 +11,25 @@ public class First_Game {
         Scanner scanner = new Scanner(System.in);
         //criamos o objeto scanner para ler a entrada do teclado
         String nome = scanner.next();
-        //escaneamos a string que view (sem pular espaço) e guardamos na variável string nome
+        //escaneamos a string que vier (sem pular espaço) e guardamos na variável string nome
 
+        String Zoio ="Zoio";
+
+        if(!nome.equals(Zoio)) //usei .equals para comparar a String nome com a String Zoio,
+        //e coloquei um ! antes para indicar quando não forem iguais na comparação
+        {
+            Player p1 = new Player(nome,15,"punhos");
+            
+            System.out.println("Olá "+ p1.getnome() +"!\nÉ um prazer te conhecer!\n");
+            System.out.println("Stats: "+p1.gethp()+ " HP\n" + "arma atual: " + p1.getarma());
+        }
+        else {
+            System.out.println("Fala AlEk o bgl é intelcore mermo garai");
+            Player p1 = new Player(nome,30,"Marreta");
+            System.out.println("Stats: "+p1.gethp()+ " HP\n" + "arma atual: " + p1.getarma());
+        }
         
-        Player p1 = new Player(nome,15,"punhos");
-
-        //colocar depois um IF para caso o nome dele seja Zoio, responda com a frase "HARDCORE GARAI"
-
-        System.out.println("Olá "+ nome +"!\nÉ um prazer te conhecer!\n");
-        System.out.println("Stats: "+p1.gethp()+ " HP\n" + "arma atual: " + p1.getarma());
-        
+        scanner.close();
+        //fechamos o scanner para evitar problemas de buffer e performance
     }
 }
