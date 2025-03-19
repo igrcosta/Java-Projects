7ef7bb6f943dfbdead1e8552d7928c3d0b1cc0aa
Igor Costa <igorcosta250705@gmail.com>
2025-03-19 20:33:57 -0300

    Implementing Weapon's weight system
    Para balancear as armas no sistema futuro de combate, já coloquei a mecânica de peso para os golpes.

--- text_game/Text_Game/src/Weapon.java
+++ text_game/Text_Game/src/Weapon.java
@@ -2,6 +2,7 @@ public class Weapon {
     private String ID;
     private int Damage;
     private int durability;
+    private String Type;
 
     //Construtor
     public Weapon(String ID){
@@ -9,23 +10,30 @@ public class Weapon {
         //o uppercase parece que ajuda pra ficar mais fácil com strings
         this.Damage = 0;
         this.durability = 0;
+        this.Type = null;
 
         //lista de armas abaixo
-        String Marreta = "Marreta";
-        String faca_s = "faca_simples";
-        
-        if (ID.equals(Marreta)){
+
+        if (ID.equals("Marreta")){
             SetDamage(30);
             SetDurability(400);
+            Set_Type("Heavy");
+        }
+
+        if(ID.equals("Espada_Genérica")){
+            SetDamage(10);
+            SetDurability(200);
+            Set_Type("Medium");
         }
     
         if(ID.equals("Faca_Simples")){
             SetDamage(5);
             SetDurability(150);
+            Set_Type("Light");
         }
     }
 
-    //getters
+    //Getters
     public String getID(){
         return ID;
     }
@@ -36,6 +44,10 @@ public class Weapon {
 
     public int getdurability(){
         return durability;
+    }
+
+    public String getType(){
+        return Type;
     }
 
     //Setters
@@ -52,12 +64,24 @@ public class Weapon {
         this.durability = durability;
     }
 
+    public void Set_Type(String Type){
+        this.Type = Type;
+    }
+
     //métodos extras
 
     public void Golpe(){
-        //causar dano no alvo, desgastar arma
+        //verificar o tipo da arma, para poder balancear
+        //armas pesadas, vão dar 1 golpe por rodada
+        //armas médias vao dar 2 golpes por rodadas
+        //armas leves vão dar 3 golpes por rodada
 
-        //parte para desgastar arma
+        if(Type.equals("Heavy")){
+            //1 tapa
+            //desgasta arma
+
+            for(int i=1;i!=0;i--){
+            //parte para desgastar arma
         if(durability > 0){
             durability -= 5;
             //diminuir vida de inimigo
@@ -66,5 +90,40 @@ public class Weapon {
         } else if (durability == 0){
             System.out.println("Arma quebrada, nenhum dano causado");
         }
+    }
+        } else if(Type.equals("Medium")){
+            //2 tapas
+            //desgasta arma em cada tapa
+            
+            for(int i=2;i!=0;i--){
+                //parte para desgastar arma
+            if(durability > 0){
+                durability -= 5;
+                //diminuir vida de inimigo
+                System.out.println("Você causou "+ Damage +" de dano no alvo");
+                System.out.println("Durabilidade atual da arma: "+durability);
+            } else if (durability == 0){
+                System.out.println("Arma quebrada, nenhum dano causado");
+            }
+        }
+
+        } else if(Type.equals("Light")){
+            //3 tapas
+            //desgasta arma em cada tapa
+
+            for(int i=3;i!=0;i--){
+                //parte para desgastar arma
+            if(durability > 0){
+                durability -= 5;
+                //diminuir vida de inimigo
+                System.out.println("Você causou "+ Damage +" de dano no alvo");
+                System.out.println("Durabilidade atual da arma: "+durability);
+            } else if (durability == 0){
+                System.out.println("Arma quebrada, nenhum dano causado");
+            }
+        }
+        }
+
+        //causar dano no alvo, desgastar arma
     }
 }
